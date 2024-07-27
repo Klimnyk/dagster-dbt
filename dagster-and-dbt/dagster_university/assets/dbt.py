0a7985e6411429b3ab641a6db9bc69a5215d2ab5
Klimnyk <m.klimnyk@gmail.com>
2024-07-27 22:43:27 +0300

    Lesson 5: Adding dependencies and automation to dbt models

--- dagster-and-dbt/dagster_university/assets/dbt.py
+++ dagster-and-dbt/dagster_university/assets/dbt.py
@@ -1,11 +1,19 @@
+from pathlib import Path
 from dagster import AssetExecutionContext
 from dagster_dbt import dbt_assets, DbtCliResource
+from ..resources import dbt_resource
+import os
 
 from .constants import DBT_DIRECTORY
 
 
-dbt_manifest_path = DBT_DIRECTORY.joinpath("target", "manifest.json")
-
+if os.getenv("DAGSTER_DBT_PARSE_PROJECT_ON_LOAD"):
+    dbt_manifest_path = (
+        dbt_resource.cli(["--quiet", "parse"]).wait()
+        .target_path.joinpath("manifest.json")
+    )
+else:
+    dbt_manifest_path = os.path.join(DBT_DIRECTORY, "target", "manifest.json")
 
 @dbt_assets(
     manifest=dbt_manifest_path,
