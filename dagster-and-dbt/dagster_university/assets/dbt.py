825d3c0dda558b772061a563c4cc7497cd53ab82
Klimnyk <m.klimnyk@gmail.com>
2024-07-28 16:12:05 +0300

    Lesson 7: Deploying to production

--- dagster-and-dbt/dagster_university/assets/dbt.py
+++ dagster-and-dbt/dagster_university/assets/dbt.py
@@ -5,10 +5,15 @@ from dagster_dbt import dbt_assets, DbtCliResource
 from ..resources import dbt_resource
 from dagster_dbt  import DagsterDbtTranslator
 from dagster   import AssetKey
+from ..partitions import daily_partition
+import json
 
 import os
 
 from .constants import DBT_DIRECTORY
+
+
+INCREMENTAL_SELECTOR = "config.materialized:incremental"
 
 class CustomizedDagsterDbtTranslator(DagsterDbtTranslator):
 
@@ -39,7 +44,27 @@ else:
 
 @dbt_assets(
     manifest=dbt_manifest_path,
-    dagster_dbt_translator=CustomizedDagsterDbtTranslator()
+    dagster_dbt_translator=CustomizedDagsterDbtTranslator(),
+    exclude=INCREMENTAL_SELECTOR, 
 )
 def dbt_analytics(context: AssetExecutionContext, dbt: DbtCliResource):
     yield from dbt.cli(["build"], context=context).stream()
+
+
+@dbt_assets(
+    manifest=dbt_manifest_path,
+    dagster_dbt_translator=CustomizedDagsterDbtTranslator(),
+    select=INCREMENTAL_SELECTOR,     # select only models with INCREMENTAL_SELECTOR
+    partitions_def=daily_partition   # partition those models using daily_partition
+)
+def incremental_dbt_models(
+    context: AssetExecutionContext,
+    dbt: DbtCliResource
+):
+  
+    time_window = context.partition_time_window
+    dbt_vars = {
+        "min_date": time_window.start.strftime('%Y-%m-%d'),
+        "max_date": time_window.end.strftime('%Y-%m-%d')
+    }
+    yield from dbt.cli(["build", "--vars", json.dumps(dbt_vars)], context=context).stream()
